f1ed5944ec04b9aa84609395496ae8bbf20808fb
Lê Xuân Hoàng <hoanglexuan5@gmail.com>
2023-06-03 02:27:14 +0700

    fix api gateway and Dockerfile

--- api-gateway/opa/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget -q -O opa https://github.com/open-policy-agent/opa/releases/download/v0.53.0/opa_linux_amd64
-
-chmod +x opa
--- product_service/conf.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from pydantic import BaseSettings
-
-
-class Settings(BaseSettings):
-    ACCESS_TOKEN_DEFAULT_EXPIRE_MINUTES: int = 360
-    GATEWAY_TIMEOUT: int = 59
-settings = Settings()
