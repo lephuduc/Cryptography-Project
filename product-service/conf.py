4060e8ede30c572b936bf5f638656d1e068c43f5
U-DESKTOP-D0BAH76\PkNova <pknova76@gmail.com>
2023-06-07 23:50:00 +0700

    changes to product-service

--- product-service/conf.py
+++ product-service/conf.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import os
 
 from pydantic import BaseSettings
@@ -5,4 +6,14 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     ACCESS_TOKEN_DEFAULT_EXPIRE_MINUTES: int = 360
     GATEWAY_TIMEOUT: int = 59
+=======
+import os
+
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    ACCESS_TOKEN_DEFAULT_EXPIRE_MINUTES: int = 360
+    GATEWAY_TIMEOUT: int = 59
+>>>>>>> 859e605 (changes to product-service)
 settings = Settings()
